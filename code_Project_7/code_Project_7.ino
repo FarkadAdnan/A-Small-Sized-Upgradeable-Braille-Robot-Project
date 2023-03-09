5ff54110faefba5f125a04543395815157fd7e67
Farkad Adnan <35774039+FarkadAdnan@users.noreply.github.com>
2023-03-09 15:55:17 +0300

    Delete code_Project_7 directory

--- code_Project_7/code_Project_7.ino
+++
@@ -1,9 +0,0 @@
-/*
- By:Farkad Adnan
- E-mail: farkad.hpfa95@gmail.com
- inst : farkadadnan
- #farkadadnan , #farkad_adnan , فرقد عدنان#
- FaceBook: كتاب عالم الاردوينو
- inst : arduinobook
- #كتاب_عالم_الاردوينو  #كتاب_عالم_الآردوينو 
- */
